9883965e451c8d82e9c4f0c4c5e8a0789f4f6d27
alex bisceglie <me@alexbisceglie.com>
2011-03-24 12:57:46 -0700

    removing depracated bits for re-commit

--- ffdemo/markup/templatetags/gml_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='normalize_point')
-def normalize_point(value,arg):
-    ret_val = float(value)/arg
-    return str(round(ret_val, 2)).replace(',','.')
--- ffdemo/static/assets/js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function ()
-{
-	$("#community").hover(function ()
-	{
-		$("#coming-soon-tip").toggle();
-	});
-});
-
