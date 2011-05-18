442d2da6857926daeadd7bb3b1daa1ea181e9897
brez <jbresnik@barbariangroup.com>
2011-05-18 16:59:17 -0400

    adding indexes for markup table

---
+++ ffdemo/markup/migrations/0014_add_indexes_to_markup_mark.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+       db.execute("CREATE INDEX duplicate_check_index ON markup_mark(duplicate_check)")
+       db.execute("CREATE INDEX contributor_locale_index ON markup_mark(contributor_locale)")
+       db.execute("CREATE INDEX contributor_index ON markup_mark(contributor)")
+       db.execute("CREATE INDEX reference_index ON markup_mark(reference)")
+
+
+    def backwards(self, orm):
+        raise RuntimeError("Cannot reverse this migration.")
+
+    models = {
+        'markup.mark': {
+            'Meta': {'object_name': 'Mark'},
+            'contributor': ('django.db.models.fields.CharField', [], {'max_length': '75', 'null': 'True', 'blank': 'True'}),
+            'contributor_locale': ('django.db.models.fields.CharField', [], {'max_length': '5', 'null': 'True', 'blank': 'True'}),
+            'country_code': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'date_drawn': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'duplicate_check': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'flaggings': ('django.db.models.fields.IntegerField', [], {'default': '0', 'db_index': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'points_obj': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'points_obj_simplified': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'reference': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '50', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['markup']
