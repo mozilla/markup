8f28b15c7ec272d25c49cd74f43cfbc6fa9e89a9
Fred Wenzel <fwenzel@mozilla.com>
2011-06-28 08:42:04 -0700

    Handle broken marks with width or height 0 gracefully. Bug 665764.

--- ffdemo/markup/templatetags/gml_extras.py
+++ ffdemo/markup/templatetags/gml_extras.py
@@ -6,5 +6,9 @@ register = template.Library()
 
 @register.filter(name='normalize_point')
 def normalize_point(value, arg):
+    # Division by zero is evil, so we're not doing anything in that case.
+    if not arg:
+        return 0
+
     ret_val = float(value) / arg
     return str(round(ret_val, 2)).replace(',', '.')
